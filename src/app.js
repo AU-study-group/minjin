5bb78599e2ed3e729fe0c591eea049e6fd02acdf
minjin00 <dnfenskaao@naver.com>
2023-05-07 20:56:18 +0900

    FlexBox

---
+++ src/app.js
@@ -0,0 +1,3 @@
+import {minjin} from "./components/pages/Home.js"; //모듈 불러오기
+//함수 두개면 , 로 구분 / 경로가 다르면 import 한줄 더 적음
+minjin();
---
+++ src/components/pages/Home.js
@@ -0,0 +1,3 @@
+export function minjin() {
+	console.log("모듈화 성공");
+}
